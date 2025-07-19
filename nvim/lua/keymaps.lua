608207188ca4d4cff95cf776cd0d8bf3b1785408
tsegaye27 <ttalegn29@gmail.com>
2025-07-19 09:15:06 +0300

    refactor: consolidate keymap definitions and improve readability

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,65 +1,64 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<C-s>', '<cmd>w<CR>', { desc = 'Save file' })
-vim.keymap.set('i', '<C-s>', '<Esc><cmd>w<CR>a', { desc = 'Save file' })
-vim.keymap.set('n', '<C-q>', '<cmd>q<CR>', { desc = 'Quit Neovim' })
-vim.keymap.set('n', '<C-S-q>', '<cmd>qa!<CR>', { desc = 'Force Quit Neovim' })
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
--- Use 'jk' to exit insert mode
-vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+-- General
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Use 'Y' to yank (copy) to the end of the line
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
+-- Window Navigation
+map("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+map("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+map("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Keybindings for folding
-vim.keymap.set('n', 'zc', 'zc', { desc = 'Close fold' }) -- Collapse current fold
-vim.keymap.set('n', 'zo', 'zo', { desc = 'Open fold' }) -- Open current fold
-vim.keymap.set('n', 'za', 'za', { desc = 'Toggle fold' }) -- Toggle fold open/close
-vim.keymap.set('n', 'zR', 'zR', { desc = 'Open all folds' }) -- Open all folds
-vim.keymap.set('n', 'zM', 'zM', { desc = 'Close all folds' }) -- Close all folds
+-- File Operations
+map({ "n", "v" }, "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
+map("i", "<C-s>", "<Esc><cmd>w<CR>a", { desc = "Save file" })
+map("n", "<C-q>", "<cmd>q<CR>", { desc = "Quit Neovim" })
+map("n", "<C-S-q>", "<cmd>qa!<CR>", { desc = "Force Quit Neovim" })
 
--- Spectre: Search and replace in all files
-vim.keymap.set('n', '<leader>sR', function()
-  require('spectre').open()
-end, { desc = '[S]earch and [R]eplace in all files' })
+-- Editing Enhancements
+map("i", "jk", "<Esc>", opts) -- Fast exit from insert mode
+map("i", "kj", "<Esc>", opts) -- Fast exit from insert mode
+map("n", "Y", "y$", { desc = "Yank to end of line" }) -- Make Y behave consistently
 
--- Lazygit
-vim.keymap.set('n', '<leader>gg', function()
-  local Terminal = require('toggleterm.terminal').Terminal
-  local lazygit = Terminal:new {
-    cmd = 'lazygit',
-    dir = vim.fn.systemlist('git rev-parse --show-toplevel')[1] or vim.fn.getcwd(),
-    hidden = true,
-    direction = 'float',
-    float_opts = {
-      border = 'curved',
-    },
-    on_open = function(term)
-      vim.cmd 'startinsert!'
-    end,
-    on_close = function(term)
-      vim.cmd 'startinsert!'
-    end,
-  }
-  lazygit:toggle()
-end, { desc = '[G]it [G]UI (Lazygit)' })
+-- Folding
+map("n", "zo", "zo", { desc = "Open fold" })
+map("n", "zc", "zc", { desc = "Close fold" })
+map("n", "za", "za", { desc = "Toggle fold" })
+map("n", "zR", "zR", { desc = "Open all folds" })
+map("n", "zM", "zM", { desc = "Close all folds" })
 
--- Emmet keybinding
-vim.keymap.set('i', '<C-y>,', '<C-y>,', { desc = 'Trigger Emmet' })
+-- Plugin Keymaps (Examples - some are defined in init.lua within plugin configs)
+map("n", "<leader>sR", function()
+	require("spectre").open()
+end, { desc = "[S]earch and [R]eplace in files (Spectre)" })
 
--- Toggle tag highlighting
-vim.keymap.set('n', '<leader>th', '<cmd>TSHighlightCapturesUnderCursor<CR>', { desc = '[T]oggle [H]ighlight' })
+map("n", "<leader>gg", function()
+	local Terminal = require("toggleterm.terminal").Terminal
+	local lazygit = Terminal:new({
+		cmd = "lazygit",
+		dir = vim.fn.systemlist("git rev-parse --show-toplevel")[1] or vim.fn.getcwd(),
+		hidden = true,
+		direction = "float",
+		float_opts = { border = "curved" },
+		on_open = function(_)
+			vim.cmd("startinsert!")
+		end,
+		on_close = function(_)
+			vim.cmd("stopinsert!")
+		end, -- Use stopinsert on close
+	})
+	lazygit:toggle()
+end, { desc = "[G]it [G]UI (Lazygit)" })
 
--- Toggle fold
-vim.keymap.set('n', '<leader>tf', 'za', { desc = '[T]oggle [F]old' })
+-- Emmet trigger (if needed explicitly, often handled by completion)
+-- map('i', '<C-y>,', '<C-y>,', { desc = 'Trigger Emmet' })
+
+-- Treesitter Textobjects (Examples - see Treesitter config for more)
+-- map('o', 'if', ':<C-U>lua require("nvim-treesitter.textobjects.select").select_textobject("@function.inner")<CR>', { desc = 'Select inner function' })
+-- map('x', 'if', ':<C-U>lua require("nvim-treesitter.textobjects.select").select_textobject("@function.inner")<CR>', { desc = 'Select inner function' })
+
+-- ToggleTerm mapping (defined in opts, but could be here too)
+-- map('n', '<c-\>', '<cmd>ToggleTerm<cr>', { desc = 'Toggle Terminal' })
